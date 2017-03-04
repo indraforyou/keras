d433259a6c092b232954e57022a62579995575a1
Indranil Sur <indranil.sur@asu.edu>
2017-03-03 20:21:54 -0700

    --sync--

--- examples/test_recur_mem.py
+++ examples/test_recur_mem.py
@@ -1,12 +1,55 @@
+from keras.datasets import mnist
 from keras.models import Sequential, Model
-from keras.layers import RecurDataMem, Input, Dense, TimeDistributed
+from keras.layers import RecurDataMem, Input, Dense, TimeDistributed, InputLayer
+from keras import backend as K
 
+import numpy as np
+import matplotlib.pyplot as plt
 
 # inp = RecurDataMem(time_dim=10, input_dim=(64,64,1))
 # out = TimeDistributed(Dense(32))(inp)
 
-inp = Input(shape=(64,64,1))
-out = RecurDataMem(time_dim=10)
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+inp = Input(shape=(28,28,1))
+out = RecurDataMem(time_dim=4)(inp)
 
 model = Model(input=inp, output=out)
-model.summary
+model.summary()
+
+func = K.function(inputs=[inp], outputs=[out], updates=model.updates)
+
+plt.ion()
+
+fig, axis = plt.subplots(nrows=1, ncols=1)
+# for ax in axis:
+# 	hnd = plt.imshow(stiched_fig(0))
+hnd = plt.imshow(np.hstack([X_train[0],X_train[1],X_train[2],X_train[3]]))
+
+
+plt.draw()
+plt.pause(00001)
+for idx in range(100):
+	data = X_train[idx][..., np.newaxis]
+	# output = model.predict(data[np.newaxis, ...])
+	output = func([data[np.newaxis, ...]])
+	# print output
+	# print output[0].shape
+	output = np.squeeze(output[0])
+	print output.shape
+	print output[0].shape
+
+	out_img = np.hstack([output[0],output[1],output[2],output[3]])
+	print out_img.shape
+	
+	hnd.set_data(out_img)
+
+	plt.draw()
+	plt.pause(00001)
+
+
+
+# model = Sequential()
+# model.add(InputLayer(input_shape=(64,64,1)))
+# model.add(RecurDataMem(time_dim=10))
+# model.summary()
--- keras/layers/recur_mem.py
+++ keras/layers/recur_mem.py
@@ -1,6 +1,5 @@
 from keras import backend as K
 from keras.engine.topology import Layer
-import numpy as np
 
 class RecurDataMem(Layer):
     def __init__(self,
@@ -17,18 +16,29 @@ class RecurDataMem(Layer):
 
     def build(self, input_shape):
         # Create a trainable weight variable for this layer.
-        print input_shape
-        self.W = []
-        for _ in range(self.time_dim):
-            self.W.append(self.add_weight(shape=(input_shape[1], self.time_dim),
-                                 initializer='random_uniform',
-                                 trainable=False))
-        super(RecurDataMem, self).build()
+        # self.input_shape = input_shape
+        self.out_shape = (self.time_dim,)+input_shape[1:]
+        self.W = K.zeros(self.out_shape)
+        # self.W = []
+        # for _ in range(self.time_dim):
+        #     # self.W.append(self.add_weight(shape=(input_shape[1], self.time_dim),
+        #     #                      initializer='random_uniform',
+        #     #                      trainable=False))
+        #     self.W.append(K.zeros((1,)+input_shape[1:]))
+        # # super(RecurDataMem, self).build()
+        # # super(Dense, self).__init__(**kwargs)
 
-    def call(self, x):
-        return K.dot(x, self.W)
+    def call(self, x, mask=None):
+        # for idx in range(self.time_dim-1):
+        #     self.W[idx] = self.W[idx+1]
+        # # return K.dot(x, self.W)
+        # return K.concatenate(self.W[1:]+[x], axis=0)
+        out_list = K.tf.unpack(self.W)
+        out = K.stack(out_list[1:]+[x[0]])
+        self.add_update([(self.W, out)])
+        return out
 
     def get_output_shape_for(self, input_shape):
-        return (input_shape[0], self.output_dim)
+        return self.out_shape
 
 
